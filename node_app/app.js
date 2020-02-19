3deb166f77a178d0f925f81d77b1e262371c34bd
Marcos Soares <marcos.soares@4yousee.com.br>
2020-02-19 14:18:39 -0300

    2º Parte Node e Proxy Reverso

---
+++ node_app/app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const apiRouter = express.Router()
+
+const APPLICATION = "MYAPP"
+
+const app = express()
+
+apiRouter.get("/healthCheck", (req, res) =>{
+    res.status(200).json({
+        message : "api is running GET"
+    })
+})
+
+app.use('/api/v1', apiRouter)
+
+app.listen(3000, () =>{
+    console.log(`LISTEN ${APPLICATION} PORT 3000`)
+})
